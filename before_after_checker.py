0f07c1befe862c5f16bb9b8b3ebf6bf7d59d88b0
IftitakhulZakiah <13515114@std.stei.itb.ac.id>
2020-07-22 10:05:34 +0700

    checker edited

--- before_after_checker.py
+++ before_after_checker.py
@@ -7,92 +7,50 @@ def get_args():
     parser = argparse.ArgumentParser(description="Get content for each element in input",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("qa_csv", type=str, metavar="The sorted QA CSV file input with 3 columns, 'ID', 'Kata yang salah', and 'Perbaikan'")
-    parser.add_argument("diff_file", type=str, metavar="The diff file format")
+    parser.add_argument("diff_file", type=str, metavar="The diff CSV file input with 3 columns, 'ID', 'Before', and 'After'")
+    parser.add_argument("output", type=str, metavar="The text output file")
     # parser.add_argument("per_utt", type=str, metavar="The per_utt format with 'hyp' as before version and 'ref' as after version")
-    # parser.add_argument("output", type=str, metavar="The text output file")
+    
     
     return parser.parse_args()
 
-def read_per_utt(filename):
-	i = 1
-	bef_aft_objs = {} # the before after objects
-	curr_obj = {}
-
-	file = open(filename, 'r')
-	for line in file:
-		curr_id = line.split()[0]
-		if i % 4 == 1:
-			curr_obj['ref'] = line.replace(curr_id + ' ref', '').strip()
-		elif i % 4 == 2:
-			curr_obj['hyp'] = line.replace(curr_id + ' hyp', '').strip()
-		elif i % 4== 3:
-			curr_obj['op'] = line.replace(curr_id + ' op', '').strip()
-		else:
-			curr_obj['csid'] = line.replace(curr_id + ' #csid', '').strip()
-			bef_aft_objs[curr_id] = curr_obj
-			curr_obj = {}
-		i += 1
-
-	file.close()
-
-	return bef_aft_objs
-
-def get_indexes(elmt, curr_list):
+def get_match_before(row_df_qa, df_diff):
+	""" 
+	Get the match row in df_diff based on "ID" and "Kata yang salah" from row_df_qa.
+	:param row_df_qa: row of QA dataframe.
+	:param df_diff: diff dataframe.
+	:return: list of index(int) on df_diff and list of boolean
+	"""
 	idxs = []
-	try:
-		idxs = [i for i, token in enumerate(curr_list) if token == elmt]	
-	except:
-		pass
-	return idxs
+	are_correct = []
+	for i in range(len(df_diff)):
+		if df_diff.iloc[i]['ID'] == row_df_qa['ID']:
+			if df_diff.iloc[i]['Before'] == row_df_qa['Kata yang salah']:
+				idxs.append(i)
+				are_correct.extend([1] if df_diff.iloc[i]['After'] == row_df_qa['Perbaikan'] else [0])
 
-# def read_diff_file(filename):
-# 	file = open(filename, 'r')
-	# diff = {}
-	# for line in file:
-	# 	diff_key = diff.keys()
-	# 	temp = line.split('|').strip()
+	return idxs, are_correct
 
-	# 	if temp[0] in diff_key:
-	# 		diff[temp[0]]['before'].append(temp[1])
-	# 		diff[temp[0]]['after'].append(temp[2])
-	# 	else:
-	# 		diff[temp[0]] = {'before': [temp[1]], 
-	# 						'after' : [temp[2]]}
-
-	return diff
 
 if __name__ == '__main__':
 	args = get_args()
-	qa_results = pd.read_csv(args.qa_csv)
-	diff = pd.read_csv(args.diff_file)
-	# print(qa_results['ID'][0])
-	# print(qa_results.iloc[0]['ID'])
+	qa_logs = pd.read_csv(args.qa_csv)
+	df_diff = pd.read_csv(args.diff_file)
 
-	# bef_aft_objs = read_per_utt(args.per_utt)
-	# diff = read_diff_file(args.diff_file)
+	correct_correction = {}
+	incorrect_correction = {}
 
-	for i in range(len(qa_results)):
-		curr_id = qa_results.iloc[i]['ID']
-		curr_bef = qa_results.iloc[i]['Kata yang salah']
-		curr_aft = qa_results.iloc[i]['Perbaikan']
+	with open(args.output, 'a') as f:
+		for i in range(len(qa_logs)):
+			curr_row = qa_logs.iloc[i]
+			idxs, are_correct = get_match_before(curr_row, df_diff)
 
-		# curr_obj = bef_aft_objs[curr_id]
-		# curr_hyp = curr_obj['hyp'].split().strip()
-		# curr_ref = curr_obj['ref'].split().strip()
-
-		# idxs_hyp = get_indexes(curr_bef, curr_hyp)
-		# idxs_ref = get_indexes(curr_aft, curr_ref)
-
-		# for j in range(len(idxs_hyp)):
-		# 	id_hyp = idxs_hyp[j]
-		# 	id_ref = idxs_ref[j]
-		#############################################	
-		# diff_bef = diff[curr_id]['before']
-		# diff_aft = diff[curr_id]['after']
-
-		# idxs_hyp = get_indexes(curr_bef, diff_bef)
-		# idxs_ref = get_indexes(curr_aft, diff_aft)
-
-		
-		for 
-		if curr_bef == diff_bef
+			for j in range(len(are_correct)):
+				if are_correct[j] == 0:
+					f.write("Incorrect correction ({}). In QA logs : {}, but in new version : {}. Please check it again\n"\
+						.format(curr_row['ID'], curr_row['Perbaikan'] , df_diff.iloc[idxs[j]]['After']))
+					print("Incorrect")
+				else:
+					f.write("Correct correction ({}). In QA logs & new version : {}\n"\
+						.format(curr_row['ID'], curr_row['Perbaikan']))
+					print("Correct")
